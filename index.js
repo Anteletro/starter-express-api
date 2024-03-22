44f9064d26629e9e9e33040780a092f3577675b0
Anteletro <89053647+Anteletro@users.noreply.github.com>
2024-03-22 12:29:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ app.all('/', (req, res) => {
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
+
+
+const { Web3 } = require("web3");
+const web3 = new Web3(
+  "https://optimism-sepolia.infura.io/v3/9b7be415fa304a86acaad068dee45744"
+);
+
+const abi =
+  require("/home/kauanluacio/Servidor/build/contracts/newNFT.json").abi;
+const contractAddress = "0xb5aad160cad5ee45d1af0ea4db02974caf8c0c35"; 
+
